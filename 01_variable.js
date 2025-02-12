67fd73726049fb5557b1151c4bd9862e3d4a8dd7
smitpatel <smitp5281@gmail.com>
2025-02-12 21:35:23 +0530

    first commit

---
+++ 01_variable.js
@@ -0,0 +1,14 @@
+// console.log("Here You Can Print Anything...!")
+
+const accountid = 76122351 
+let a = "jaipur"
+let accout_pass = "Hello_72e6"
+state = "Gujarat"
+
+let UML 
+/*
+You Can Use Me As a Comment
+*/
+console.log(a)
+console.table([accountid , a , accout_pass, state , UML])
+
